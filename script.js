3e74661d49a2c77a71d6ba6293de2901854ab39f
Ritmo <jajh102098@gmail.com>
2023-02-03 08:29:26 +0800

    Feature: Favicon, Navbar, and Theme Toggle

---
+++ script.js
@@ -0,0 +1,17 @@
+// Pre-loader
+const fade = () => {
+    const wrapper = 
+    document.querySelector('.loader');
+    wrapper.classList.add('fade');
+};
+window.addEventListener('load', fade);
+
+// Set color scheme
+const setTheme = () => {
+    var theme = document.documentElement.getAttribute("data-theme");
+    if(theme == "dark") theme = "light";
+    else if(theme == "light") theme = "dark";
+
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("data-theme", theme);
+}
