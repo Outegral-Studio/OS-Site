d255606047db997ed3bbcf05423f2abb7102a316
Ritmo <jajh102098@gmail.com>
2023-02-08 15:28:46 +0800

    Test: New theme toggle logic; Preloader

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+// Detect color scheme
+// Order: Local Storage > OS theme
+const detectColorScheme = () => {
+    // Defaults as dark
+    var theme = "dark";
+
+    // ...if OS theme is light
+    if(window.matchMedia("(prefers-color-scheme: light)").matches)
+        var theme = "light";
+    // ...if local storage is light
+    if(localStorage.getItem("data-user-theme") == "light")
+        var theme = "light";
+    else
+        var theme = "dark";
+
+    document.documentElement.setAttribute("data-user-theme", theme);
+}
+detectColorScheme();
+
+// Pre-loader
+const fade = () => {
+    const wrapper = document.querySelector('.loader');
+    wrapper.classList.add('fade');
+}
+window.addEventListener('load', fade);
+
+// Button: Toggle color scheme
+const toggleTheme = () => {
+    var theme = document.documentElement.getAttribute("data-user-theme");
+    if(theme == "dark") theme = "light";
+    else if(theme == "light") theme = "dark";
+
+    document.documentElement.setAttribute("data-user-theme", theme);
+    localStorage.setItem("data-user-theme", theme);
+}
