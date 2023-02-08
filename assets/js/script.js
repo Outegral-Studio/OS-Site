a66b1136992911ad4bcbc0bc8a949cd53cd19e17
Ritmo <jajh102098@gmail.com>
2023-02-08 16:29:02 +0800

    Update: Color theme

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,12 +8,12 @@ const detectColorScheme = () => {
     if(window.matchMedia("(prefers-color-scheme: light)").matches)
         var theme = "light";
     // ...if local storage is light
-    if(localStorage.getItem("data-theme") == "light")
+    if(localStorage.getItem("data-user-theme") == "light")
         var theme = "light";
     else
         var theme = "dark";
 
-    document.documentElement.setAttribute("data-theme", theme);
+    document.documentElement.setAttribute("data-user-theme", theme);
 }
 detectColorScheme();
 
@@ -26,10 +26,10 @@ window.addEventListener('load', fade);
 
 // Button: Toggle color scheme
 const toggleTheme = () => {
-    var theme = document.documentElement.getAttribute("data-theme");
+    var theme = document.documentElement.getAttribute("data-user-theme");
     if(theme == "dark") theme = "light";
     else if(theme == "light") theme = "dark";
 
-    document.documentElement.setAttribute("data-theme", theme);
-    localStorage.setItem("data-theme", theme);
+    document.documentElement.setAttribute("data-user-theme", theme);
+    localStorage.setItem("data-user-theme", theme);
 }
